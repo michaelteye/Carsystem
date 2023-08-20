57d0785f07f794cdaef90d0d7b9bdac1d688f0e1
michaelteye <michaelteyegyemah@gmail.com>
2023-08-20 22:36:44 +0000

    finishing the test but a adding a little env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule, ConfigService } from '@nestjs/config'
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import cookieSession from 'cookie-session';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService]
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
       entities: [User, Report],
       synchronize: true,
     }),
@@ -19,6 +29,23 @@ import { Report } from './reports/report.entity';
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true, // remove extra properties that are not defined in the DTO class (dto-validation)
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer:MiddlewareConsumer){
+    consumer.apply(
+      cookieSession({
+        keys:['asdfasfd']
+      }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import cookieSession = require('cookie-session');
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys:['asdfasfd']
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe(
-      {
-        whitelist: true, // remove extra properties that are not defined in the DTO class (dto-validation)
-      }
-    ));
-  
+  app.use(
+    
+  );
+  app.useGlobalPipes();
+
   await app.listen(3000);
 }
 bootstrap();
